03195e4bb395ee23cd7ac5e8c75fb13fed94d7a1
orangetaikiri <philipp.p.reisinger@icloud.com>
2026-04-30 08:54:18 +0000

    wip

--- src/main/java/at/fhburgenland/Person.java
+++ src/main/java/at/fhburgenland/Person.java
@@ -1,7 +1,22 @@
 package at.fhburgenland;
 
+import jakarta.persistence.*;
+
+@Entity(name = "Person")
+@Table(name= "person")
 public class Person {
-    // TODO Fields of Person
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "pnr", updatable = false, nullable = false)
+    private int pnr;
+
+    private String vorname;
+
+    private String nachname;
+
+    private Integer gehalt;
+
 
     public Person() {
         // TODO Initialization of fields of Person
