096b7debdb8a68e7e6a4cda8ab74f52a2bf78416
orangetaikiri <philipp.p.reisinger@icloud.com>
2026-04-30 09:01:27 +0000

    wip

--- src/main/java/at/fhburgenland/Person.java
+++ src/main/java/at/fhburgenland/Person.java
@@ -11,16 +11,24 @@ public class Person {
     @Column(name = "pnr", updatable = false, nullable = false)
     private int pnr;
 
+    @Column(name = "vorname", nullable = false, length = 50)
     private String vorname;
 
+    @Column(name = "nachname", nullable = false, length = 50)
     private String nachname;
 
-    private Integer gehalt;
+    @Column(name = "gehalt", nullable = false)
+    private int gehalt;
 
 
     public Person() {
         // TODO Initialization of fields of Person
     }
 
-    // TODO Implement body of Person
+    public Person(String vorname, String nachname, int gehalt){
+        this.vorname = vorname;
+        this.nachname = nachname;
+        this.gehalt = gehalt;
+    }
+
 }
