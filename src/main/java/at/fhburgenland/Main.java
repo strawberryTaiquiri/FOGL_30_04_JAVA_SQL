a6da123ca7d04304e9bf9aef3d976ab00b37515e
orangetaikiri <philipp.p.reisinger@icloud.com>
2026-04-30 10:33:35 +0000

    readall

--- src/main/java/at/fhburgenland/Main.java
+++ src/main/java/at/fhburgenland/Main.java
@@ -21,7 +21,11 @@ public class Main {
                 -) delete Person
          */
 
-        readAll();
+        // findPersonByFamilyName("Reisinger");
+        // findPersonByPnr(4);
+
+        // updatePerson("maxUpdated", "max", 777777, 1);
+        deletePerson(1);
 
         EMF.close();
     }
@@ -54,10 +58,31 @@ public class Main {
     public static void readAll(){
 
         EntityManager em = EMF.createEntityManager();
-
         String query = "SELECT p from Person p";
-
         TypedQuery<Person> tq = em.createQuery(query, Person.class);
+        List<Person> personList = null;
+
+        try{
+            personList = tq.getResultList();
+
+            for (Person person : personList) {
+                System.out.println(person.getPnr() +" "+ person.getVorname() + " "+
+                        person.getNachname() +" "+ person.getGehalt());
+            }
+        } catch (Exception ex){
+            ex.printStackTrace();
+        } finally {
+            em.close();
+        }
+    }
+
+    public static void findPersonByFamilyName(String nachname){
+
+        EntityManager em = EMF.createEntityManager();
+        String query = "SELECT p from Person p where p.nachname = :nname";
+        TypedQuery<Person> tq = em.createQuery(query, Person.class);
+
+        tq.setParameter("nname", nachname);
 
         List<Person> personList = null;
 
@@ -73,8 +98,83 @@ public class Main {
         } finally {
             em.close();
         }
+    }
 
 
+    public static void findPersonByPnr(int pnr){
 
+        EntityManager em = EMF.createEntityManager();
+        Person person;
+
+        try{
+            person = em.find(Person.class, pnr);
+            em.persist(person);
+
+            System.out.println(person.getPnr() +" "+ person.getVorname() + " "+ person.getNachname() +" "+ person.getGehalt());
+        } catch (Exception ex){
+            ex.printStackTrace();
+        } finally {
+            em.close();
+        }
+    }
+
+    public static void updatePerson(String vorname, String nachname, int gehalt, int pnr){
+
+        EntityManager em = EMF.createEntityManager();
+        EntityTransaction et = null;
+        Person person = null;
+
+        try{
+            et = em.getTransaction();
+            et.begin();
+
+            person = em.find(Person.class, pnr);
+
+            if(person == null){
+                throw new RuntimeException("keine Person gefuden");
+            }
+
+            person.setGehalt(gehalt);
+            person.setNachname(nachname);
+            person.setVorname(vorname);
+
+            em.persist(person);
+
+            et.commit();
+
+        } catch (Exception ex) {
+            if(et != null){
+                et.rollback();
+            }
+        } finally {
+            em.close();
+        }
+    }
+
+    public static void deletePerson(int pnr){
+
+        EntityManager em = EMF.createEntityManager();
+        EntityTransaction et = null;
+        Person person = null;
+
+        try{
+            et = em.getTransaction();
+            et.begin();
+
+            person = em.find(Person.class, pnr);
+
+            if(person == null){
+                throw new RuntimeException("keine Person gefuden");
+            }
+
+            em.remove(person);
+            et.commit();
+        } catch (Exception ex) {
+            if(et != null){
+                et.rollback();
+            }
+        } finally {
+            em.close();
+        }
     }
 }
