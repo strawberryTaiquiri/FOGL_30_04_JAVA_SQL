8d5b9594df07c7d96049b969167454cafe22a796
orangetaikiri <philipp.p.reisinger@icloud.com>
2026-04-30 09:17:59 +0000

    wip

--- src/main/java/at/fhburgenland/Main.java
+++ src/main/java/at/fhburgenland/Main.java
@@ -8,15 +8,34 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Test");
-        /* TO DO
+        /* TODO:
             -) Connect Database
             -) Klasse zur Tabelle erstellen!
             -) Create Methods for
-                -) addPerson
+                -) addPersons
                 -) readPerson
                 -) readAllPersons --> Ausgabe ganze Tabelle
                 -) update Person
                 -) delete Person
          */
+
+        EMF.close();
+    }
+
+    public static void addPerson(String vorname, String nachname, int Gehalt){
+        EntityManager em = EMF.createEntityManager();
+        EntityTransaction et = null;
+
+        try{
+
+        } catch (Exception ex){
+            System.out.println("ex = " + ex);
+        } finally {
+            em.close();
+        }
+
+
+
+
     }
 }
